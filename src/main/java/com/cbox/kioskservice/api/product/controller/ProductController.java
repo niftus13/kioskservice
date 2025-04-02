77e8acce4912bfaa5e1fcb72737d42c8cf5589af
niftus13 <niftus13@gmail.com>
2025-04-02 11:23:15 +0900

    1

--- src/main/java/com/cbox/kioskservice/api/product/controller/ProductController.java
+++ src/main/java/com/cbox/kioskservice/api/product/controller/ProductController.java
@@ -5,14 +5,14 @@ import java.util.Map;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import com.cbox.kioskservice.api.common.file.FileUploader;
 import com.cbox.kioskservice.api.common.pageDTO.PageRequestDTO;
